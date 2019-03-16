076b98581dd4f2a5ef261fcc11e95df0f256a1ec
temprory <beishengren@126.com>
2019-03-17 02:17:21 +0800

    -

--- rpcclient_pool.go
+++ rpcclient_pool.go
@@ -2,12 +2,15 @@ package net
 
 import (
 	"errors"
+	"sync/atomic"
 	"time"
 )
 
 type RpcClientPool struct {
+	idx     int64
+	size    uint64
 	codec   IRpcCodec
-	clients chan *RpcClient
+	clients []*RpcClient
 }
 
 func (pool *RpcClientPool) Codec() IRpcCodec {
@@ -15,40 +18,31 @@ func (pool *RpcClientPool) Codec() IRpcCodec {
 }
 
 func (pool *RpcClientPool) CallCmd(cmd uint32, req interface{}, rsp interface{}) error {
-	client := <-pool.clients
+	idx := atomic.AddInt64(&pool.idx, 1)
+	client := pool.clients[uint64(idx)%pool.size]
 	err := client.CallCmd(cmd, req, rsp)
-	pool.clients <- client
 	return err
 }
 
 func (pool *RpcClientPool) CallCmdWithTimeout(cmd uint32, req interface{}, rsp interface{}, timeout time.Duration) error {
-	select {
-	case client := <-pool.clients:
-		err := client.CallCmdWithTimeout(cmd, req, rsp, timeout)
-		pool.clients <- client
-		return err
-	case <-time.After(timeout):
-	}
-
-	return ErrRpcCallTimeout
+	idx := atomic.AddInt64(&pool.idx, 1)
+	client := pool.clients[uint64(idx)%pool.size]
+	err := client.CallCmdWithTimeout(cmd, req, rsp, timeout)
+	return err
 }
 
 func (pool *RpcClientPool) CallMethod(method string, req interface{}, rsp interface{}) error {
-	client := <-pool.clients
+	idx := atomic.AddInt64(&pool.idx, 1)
+	client := pool.clients[uint64(idx)%pool.size]
 	err := client.CallMethod(method, req, rsp)
-	pool.clients <- client
 	return err
 }
 
 func (pool *RpcClientPool) CallMethodWithTimeout(method string, req interface{}, rsp interface{}, timeout time.Duration) error {
-	select {
-	case client := <-pool.clients:
-		err := client.CallMethodWithTimeout(method, req, rsp, timeout)
-		pool.clients <- client
-		return err
-	case <-time.After(timeout):
-	}
-	return ErrRpcCallTimeout
+	idx := atomic.AddInt64(&pool.idx, 1)
+	client := pool.clients[uint64(idx)%pool.size]
+	err := client.CallMethodWithTimeout(method, req, rsp, timeout)
+	return err
 }
 
 func NewRpcClientPool(addr string, engine ITcpEngin, codec IRpcCodec, poolSize int, onConnected func(ITcpClient)) (*RpcClientPool, error) {
@@ -87,8 +81,9 @@ func NewRpcClientPool(addr string, engine ITcpEngin, codec IRpcCodec, poolSize i
 	}
 
 	pool := &RpcClientPool{
+		size:    uint64(poolSize),
 		codec:   codec,
-		clients: make(chan *RpcClient, poolSize*100),
+		clients: make([]*RpcClient, poolSize),
 	}
 
 	for i := 0; i < poolSize; i++ {
@@ -111,12 +106,7 @@ func NewRpcClientPool(addr string, engine ITcpEngin, codec IRpcCodec, poolSize i
 		})
 
 		clients[client] = rpcclient
-	}
-
-	for i := 0; i < 100; i++ {
-		for _, v := range clients {
-			pool.clients <- v
-		}
+		pool.clients[i] = rpcclient
 	}
 
 	return pool, nil
